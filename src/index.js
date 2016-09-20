e2c93f3f0044729f22336a1a3aac289bf97c4aa0
Efflam <Efflam.DANIEL@publicis-modem.fr>
2016-09-20 16:59:15 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-
 import React from 'react'
-import ReactDOMServer from 'react-dom/server'
+import {renderToStaticMarkup} from 'react-dom/server'
 import Root from './components/Root'
 import Body from './components/Body'
 import { renderStatic } from 'glamor/server'
 
+
+const renderBody = locals =>  
+  renderToStaticMarkup(<Body {...locals} />)
+
+const renderPage = (html, css, locals) => 
+  renderToStaticMarkup(<Root {...locals} css={css} body={html} />)
+
 const render = (locals, callback) => {
-  let { html, css, ids } = renderStatic(() =>
-    ReactDOMServer.renderToStaticMarkup(<Body {...locals} />))
-  console.log(css)
-  const page = ReactDOMServer.renderToStaticMarkup(<Root {...locals} css={css} body={html} />)
-  callback(null, '<!DOCTYPE html>' + page)
+  const { html, css } = renderStatic(() => renderBody(locals))
+  callback(null, '<!DOCTYPE html>' + renderPage(html, css, locals))
 }
 
 export default render
